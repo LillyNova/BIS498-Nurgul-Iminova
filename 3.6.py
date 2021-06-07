9c65791e4f27ccfb2d2e640738677bf31194e35d
Nurgul iminova <nui220@lehigh.edu>
2021-06-07 15:11:32 -0400

    Problem 3.6

---
+++ 3.6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  5 18:29:06 2021
+
+@author: nurguliminova
+"""
+print ('What is your problem?')
+input(" ")
+answer = input ('Have you had this problem before (yes or no)?\n')
+if answer == 'yes':
+    print ('Well,you have it again.')
+else:
+    print ('Well,you have it now')
